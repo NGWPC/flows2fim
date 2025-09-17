f0bb5639e87fc39ca447c7fcb079d707f54a5ac8
ar-siddiqui <abdulraheemsiddiqui@gmail.com>
2025-09-17 12:23:14 -0400

    Add release for linux-arm64

--- scripts/build-darwin-arm64.sh
+++ scripts/build-darwin-arm64.sh
@@ -7,6 +7,6 @@ echo "Building for Darwin (MacOS) ARM64..."
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -buildvcs=false \
     -ldflags="-X main.GitTag=$(git describe --tags --always --dirty) -X main.GitCommit=$(git rev-parse --short HEAD) -X main.BuildDate=$(date +%Y-%m-%d)" \
     -o builds/darwin-arm64/flows2fim main.go
-echo "Mac build completed."
+echo "MacOS ARM64 build completed."
 
 chmod +x builds/darwin-arm64/flows2fim
--- scripts/create-release-assets.sh
+++ scripts/create-release-assets.sh
@@ -5,10 +5,12 @@ set -eo pipefail
 
 ./scripts/build-darwin-arm64.sh
 ./scripts/build-linux-amd64.sh
+./scripts/build-linux-arm64.sh
 ./scripts/build-windows-amd64.sh
 
 echo "Creating release assets ..."
 zip -j builds/flows2fim-windows-amd64.zip builds/windows-amd64/flows2fim.exe
 tar -czvf builds/flows2fim-darwin-arm64.tar.gz -C builds/darwin-arm64 flows2fim
 tar -czvf builds/flows2fim-linux-amd64.tar.gz -C builds/linux-amd64 flows2fim
+tar -czvf builds/flows2fim-linux-arm64.tar.gz -C builds/linux-arm64 flows2fim
 echo "Release assets created"
